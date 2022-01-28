f6f1889d8449076ce453597ac80c797e6c4505cc
Nick Englezakis <95222208+nikoengl27@users.noreply.github.com>
2022-01-28 11:01:20 +0000

    added classes

---
+++ models/player.py
@@ -0,0 +1,6 @@
+class Player:
+    
+    def __init__(self, name, position, id = None):
+        self.name = name
+        self.position = position
+        self.id = id
---
+++ models/team.py
@@ -0,0 +1,7 @@
+class Team:
+    
+    def __init__(self, name, stadium, points, id = None):
+        self.name = name
+        self.stadium = stadium
+        self.points = points
+        self.id = id
